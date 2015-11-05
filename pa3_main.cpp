6d61aa3eb489a96e44f919e4f241d0b1214d7d05
Julian <jln.whitley@gmail.com>
2015-11-05 11:31:39 -0700

    fixed bugs in "calculate closest Centroid function". Resolved issues with inf value beta calculations.

--- pa3_main.cpp
+++ pa3_main.cpp
@@ -20,16 +20,23 @@ int main(void){
 
     // Open point file and output file
     string inFileName = "/Users/julianabbott-whitley/Google_Drive/School/UCD/Current_Classes/CSCI_2312_Intermediate/ucd-csci2312-pa2/data_points.txt";
-    string outFileName = "/Users/julianabbott-whitley/Google_Drive/School/UCD/Current_Classes/CSCI_2312_Intermediate/ucd-csci2312-pa2/data_clusters.txt";
+    string outFileName = "/Users/julianabbott-whitley/Google_Drive/School/UCD/Current_Classes/CSCI_2312_Intermediate/ucd-csci2312-pa2/data_clusters";
 
-    int k = 5;
-    kmeans km(5);
+    int k = 4;
+    kmeans km(k);
     km.load_main_cluster_from_file(inFileName);
     km.pick_K_point_arr();
-    km.show_K_points();
     km.create_k_clusters();
-    km.qc_K_clusters();
 
+    km.load_initial_points_into_k_clusters();
+
+    km.process_kmeans();
+
+    // Output each cluster file
+    for (int i = 0; i < k; i++) {
+        string ofName = outFileName + "_" + to_string(i) + ".txt";
+        km.write_cluster_to_file(ofName, i);
+    }
 
 
     return 0;
